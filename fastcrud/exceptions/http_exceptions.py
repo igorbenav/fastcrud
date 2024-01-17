cf505f3da6fda7baf3dda3da28738d3f43e29e8a
Igor Magalhaes <igor.magalhaes.r@gmail.com>
2024-01-17 04:58:44 -0300

    exceptions refactored to support python before 3.11

--- fastcrud/exceptions/http_exceptions.py
+++ fastcrud/exceptions/http_exceptions.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from http import HTTPStatus
 from fastapi import HTTPException, status
 
@@ -6,7 +8,7 @@ class CustomException(HTTPException):
     def __init__(
         self,
         status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR,
-        detail: str | None = None,
+        detail: Union[str, None] = None,
     ):
         if not detail:
             detail = HTTPStatus(status_code).description
@@ -14,39 +16,39 @@ class CustomException(HTTPException):
 
 
 class BadRequestException(CustomException):
-    def __init__(self, detail: str | None = None):
+    def __init__(self, detail: Union[str, None] = None):
         super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
 
 
 class NotFoundException(CustomException):
-    def __init__(self, detail: str | None = None):
+    def __init__(self, detail: Union[str, None] = None):
         super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
 
 
 class ForbiddenException(CustomException):
-    def __init__(self, detail: str | None = None):
+    def __init__(self, detail: Union[str, None] = None):
         super().__init__(status_code=status.HTTP_403_FORBIDDEN, detail=detail)
 
 
 class UnauthorizedException(CustomException):
-    def __init__(self, detail: str | None = None):
+    def __init__(self, detail: Union[str, None] = None):
         super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
 
 
 class UnprocessableEntityException(CustomException):
-    def __init__(self, detail: str | None = None):
+    def __init__(self, detail: Union[str, None] = None):
         super().__init__(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=detail
         )
 
 
 class DuplicateValueException(CustomException):
-    def __init__(self, detail: str | None = None):
+    def __init__(self, detail: Union[str, None] = None):
         super().__init__(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=detail
         )
 
 
 class RateLimitException(CustomException):
-    def __init__(self, detail: str | None = None):
+    def __init__(self, detail: Union[str, None] = None):
         super().__init__(status_code=status.HTTP_429_TOO_MANY_REQUESTS, detail=detail)

df80dd4ef9b336fcbb764a05a5fef7f266f81cd6
doubledare704 <doubledare704@gmail.com>
2025-03-27 13:12:26 +0200

    fixing types

--- fastcrud/paginated/schemas.py
+++ fastcrud/paginated/schemas.py
@@ -6,26 +6,24 @@ SchemaType = TypeVar("SchemaType", bound=BaseModel)
 
 
 def create_list_response(
-    schema: type[SchemaType], response_key: str = "data"
+    schema: Type[SchemaType], response_key: str = "data"
 ) -> Type[BaseModel]:
     """Creates a dynamic ListResponse model with the specified response key."""
-    return create_model("DynamicListResponse", **{response_key: (list[schema], ...)}) # type: ignore
+    return create_model("DynamicListResponse", **{response_key: (list[SchemaType], ...)}) # type: ignore
 
 
 def create_paginated_response(
-    schema: type[SchemaType], response_key: str = "data"
+    schema: Type[SchemaType], response_key: str = "data"
 ) -> Type[BaseModel]:
     """Creates a dynamic PaginatedResponse model with the specified response key."""
-    return create_model( # type: ignore
-        "DynamicPaginatedResponse",
-        **{
-            response_key: (list[schema], ...),
-            "total_count": (int, ...),
-            "has_more": (bool, ...),
-            "page": (Optional[int], None),
-            "items_per_page": (Optional[int], None),
-        }
-    )
+    fields = {
+        response_key: (list[SchemaType], ...),
+        "total_count": (int, ...),
+        "has_more": (bool, ...),
+        "page": (Optional[int], None),
+        "items_per_page": (Optional[int], None),
+    }
+    return create_model("DynamicPaginatedResponse", **fields)  # type: ignore
 
 
 class ListResponse(BaseModel, Generic[SchemaType]):
